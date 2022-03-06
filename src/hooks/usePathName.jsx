f67e68331a96208ef1caff1dc4b0fb77823a7501
Dayana Huaytalla <dhuaytalla@imca.edu.pe>
2022-03-06 18:14:24 -0500

    Add navbar and nomatch page

---
+++ src/hooks/usePathName.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useLocation } from 'react-router-dom'
+
+const usePathname = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log(location);
+  }, [location])
+  
+  return location.pathname;
+}
+export default usePathname;
--- src/pages/nomatch.jsx
+++ src/pages/nomatch.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const NoMatch = () => {
   return (
-    <h1>NoMatch</h1>
-  )
+    <div className='d-flex flex-column text-center pt-15'>
+      <h2>No hay nada aquí!</h2>
+      <p>
+        <Link to="/" className='text-primary fw-bold'>Ir a la página principal</Link>
+      </p>
+    </div>
+  );
 }
 
 export default NoMatch
